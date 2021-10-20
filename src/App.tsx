ecd0b160b34148e814c6871976667930d5ee2421
asosunoff <al.sosunoff@gmail.com>
2021-10-20 23:11:45 +0300

    added app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const App: React.FC = () => {
+  const store = useSelector((store) => store);
+
+  console.log(store);
+
   return <>1</>;
 };
