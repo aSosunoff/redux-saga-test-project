d746685d2ffa93cc22d5e1542e4e58d56b066264
asosunoff <al.sosunoff@gmail.com>
2021-10-22 00:06:24 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Action, Dispatch } from "redux";
+
+export type dispatchType = "LOAD_PEOPLE";
+
+const useDispatchClick = () => {
+  const dispatch = useDispatch<Dispatch<Action<dispatchType>>>();
+
+  const dispatchClick = useCallback(() => {
+    dispatch({ type: "LOAD_PEOPLE" });
+  }, [dispatch]);
+
+  return { dispatchClick };
+};
 
 export const App: React.FC = () => {
   const store = useSelector((store) => store);
 
+  const { dispatchClick } = useDispatchClick();
+
   console.log(store);
 
-  return <>1</>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatchClick();
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
 };
