96120773a951cba29391785afba342586e4266fc
asosunoff <al.sosunoff@gmail.com>
2021-10-22 00:06:41 +0300

    fix

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,5 +1,38 @@
 import { Reducer } from "redux";
+import { PersonType } from "../sagas";
 
-export const reducer: Reducer = (state = {}, action) => {
-  return state;
+type State = {
+  people: PersonType[];
+};
+
+const initialState: State = {
+  people: [],
+};
+
+export type ActionSetPeople = {
+  type: "SET_PEOPLE";
+  payload: PersonType[];
+};
+
+type Action =
+  | ActionSetPeople
+  | {
+      type: Pick<any, ActionSetPeople["type"]>;
+    };
+
+export const reducer: Reducer<State, Action> = (
+  state = initialState,
+  action
+) => {
+  switch (action.type) {
+    case "SET_PEOPLE": {
+      return {
+        ...state,
+        people: action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
 };
