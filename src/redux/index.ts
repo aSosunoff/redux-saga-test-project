923cc736f4901b61cbd069e8856791dc3d869659
asosunoff <al.sosunoff@gmail.com>
2021-10-22 00:06:35 +0300

    fix

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,9 +2,14 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { reducer } from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { rootSaga } from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const enhancer = applyMiddleware(sagaMiddleware);
 
-export const store = createStore(reducer, composeWithDevTools(enhancer));
+const store = createStore(reducer, composeWithDevTools(enhancer));
+
+sagaMiddleware.run(rootSaga);
+
+export { store };
