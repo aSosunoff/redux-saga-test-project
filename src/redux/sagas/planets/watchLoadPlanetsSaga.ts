5862c6b9ffedecb085b493eef2db069bb485778d
asosunoff <al.sosunoff@gmail.com>
2021-10-24 23:16:27 +0300

    fix

--- src/redux/sagas/planets/watchLoadPlanetsSaga.ts
+++ src/redux/sagas/planets/watchLoadPlanetsSaga.ts
@@ -1,8 +1,8 @@
 import { takeEvery } from "redux-saga/effects";
 
 import { dispatchType, type } from "../../../hooks/use-dispatch-load-planets";
-import { workerSaga } from "./workerSaga";
+import { workerLoadPlanetsSaga } from "./workerLoadPlanetsSaga";
 
 export function* watchLoadPlanetsSaga() {
-  yield takeEvery<dispatchType>(type, workerSaga);
+  yield takeEvery<dispatchType>(type, workerLoadPlanetsSaga);
 }
