73fa7e3444d06edd83d76d3a836ed9d061cd31bb
asosunoff <al.sosunoff@gmail.com>
2021-10-24 22:11:58 +0300

    fix

--- src/redux/sagas/people/workerSaga.ts
+++ src/redux/sagas/people/workerSaga.ts
@@ -1,32 +1,36 @@
-import { put, call, PutEffect, CallEffect } from "redux-saga/effects";
+import { put, call, PutEffect, CallEffect, fork } from "redux-saga/effects";
 import { Person } from "../../../interfaces/person";
 import { Planet } from "../../../interfaces/planet";
 import { SWAPIService } from "../../../services/SWAPIService";
 import { ActionSetPeople, ActionSetPlanet } from "../../reducers";
 
-export function* workerSaga(): Generator<
-  | CallEffect<Person[]>
-  | CallEffect<Planet[]>
-  | PutEffect<ActionSetPeople>
-  | PutEffect<ActionSetPlanet>,
+export function* loadPeople(): Generator<
+  CallEffect<Person[]> | PutEffect<ActionSetPeople>,
   void,
-  Person[] | Planet[]
+  Person[]
 > {
   const persons = yield call(SWAPIService.getPersons);
-
-  console.log(persons);
-
-  const planets = yield call(SWAPIService.getPlanets);
-
-  console.log(planets);
 
   yield put<ActionSetPeople>({
     type: "SET_PEOPLE",
     payload: persons as Person[],
   });
+}
+
+export function* loadPlanets(): Generator<
+  CallEffect<Planet[]> | PutEffect<ActionSetPlanet>,
+  void,
+  Planet[]
+> {
+  const planets = yield call(SWAPIService.getPlanets);
 
   yield put<ActionSetPlanet>({
     type: "SET_PLANETS",
     payload: planets as Planet[],
   });
+}
+
+export function* workerSaga() {
+  yield fork(loadPeople);
+  yield fork(loadPlanets);
 }
