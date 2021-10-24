d69730b47fcb6106887957a1dd58c49139477112
asosunoff <al.sosunoff@gmail.com>
2021-10-24 21:52:21 +0300

    fix workerSga

--- src/redux/sagas/people/workerSaga.ts
+++ src/redux/sagas/people/workerSaga.ts
@@ -1,14 +1,32 @@
 import { put, call, PutEffect, CallEffect } from "redux-saga/effects";
+import { Person } from "../../../interfaces/person";
+import { Planet } from "../../../interfaces/planet";
 import { SWAPIService } from "../../../services/SWAPIService";
-import { ActionSetPeople } from "../../reducers";
-import { Person } from "./interfaces";
+import { ActionSetPeople, ActionSetPlanet } from "../../reducers";
 
 export function* workerSaga(): Generator<
-  CallEffect<Person[]> | PutEffect<ActionSetPeople>,
+  | CallEffect<Person[]>
+  | CallEffect<Planet[]>
+  | PutEffect<ActionSetPeople>
+  | PutEffect<ActionSetPlanet>,
   void,
-  Person[]
+  Person[] | Planet[]
 > {
-  const payload = yield call(SWAPIService.getPerson);
+  const persons = yield call(SWAPIService.getPersons);
 
-  yield put<ActionSetPeople>({ type: "SET_PEOPLE", payload });
+  console.log(persons);
+
+  const planets = yield call(SWAPIService.getPlanets);
+
+  console.log(planets);
+
+  yield put<ActionSetPeople>({
+    type: "SET_PEOPLE",
+    payload: persons as Person[],
+  });
+
+  yield put<ActionSetPlanet>({
+    type: "SET_PLANETS",
+    payload: planets as Planet[],
+  });
 }
