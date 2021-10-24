1ffbee86ec58a66c015299679d56f929d68c697d
asosunoff <al.sosunoff@gmail.com>
2021-10-24 23:38:03 +0300

    delete

--- src/redux/sagas/allData/workerSaga.ts
+++
@@ -1,9 +0,0 @@
-import { fork } from "redux-saga/effects";
-import { workeLoadPeopleSaga } from "../people/workeLoadPeopleSaga";
-import { workerLoadPlanetsSaga } from "../planets/workerLoadPlanetsSaga";
-
-export function* workerSaga() {
-  yield fork(workeLoadPeopleSaga);
-
-  yield fork(workerLoadPlanetsSaga);
-}
