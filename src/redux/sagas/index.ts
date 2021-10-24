b18697ae0d69bb2aac7e40e60814a8d516152159
asosunoff <al.sosunoff@gmail.com>
2021-10-24 23:10:19 +0300

    fix root saga

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,6 +1,12 @@
 import { fork } from "redux-saga/effects";
+import { watchLoadAllData } from "./allData/watchLoadAllData";
 import { watchLoadPeopleSaga } from "./people/watchLoadPeopleSaga";
+import { watchLoadPlanetsSaga } from "./planets/watchLoadPlanetsSaga";
 
 export function* rootSaga() {
   yield fork(watchLoadPeopleSaga);
+
+  yield fork(watchLoadPlanetsSaga);
+
+  yield fork(watchLoadAllData);
 }
