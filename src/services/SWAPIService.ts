87316a608d0c77328abc523a1f6107c0ad4aa733
asosunoff <al.sosunoff@gmail.com>
2021-10-24 21:52:36 +0300

    added service planet

--- src/services/SWAPIService.ts
+++ src/services/SWAPIService.ts
@@ -1,14 +1,27 @@
-import { Person } from "../redux/sagas/people/interfaces";
+import { Person } from "../interfaces/person";
+import { Planet } from "../interfaces/planet";
+
+const getData = async <Result>(type: string) => {
+  const request = await fetch(`https://swapi.dev/api/${type}/`);
+
+  const data = await request.json();
+
+  return data as Result;
+};
 
 export class SWAPIService {
-  static getPerson = async () => {
-    const request = await fetch(`https://swapi.dev/api/people/`);
-
-    const data = await request.json();
-
-    const { results } = data as {
+  static getPersons = async () => {
+    const { results } = await getData<{
       results: Person[];
-    };
+    }>("people");
+
+    return results;
+  };
+
+  static getPlanets = async () => {
+    const { results } = await getData<{
+      results: Planet[];
+    }>("planets");
 
     return results;
   };
