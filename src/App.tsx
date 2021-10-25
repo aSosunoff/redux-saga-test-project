dcf96fdef0aed48bfe344d1ebf4e9118d4eff4e8
asosunoff <al.sosunoff@gmail.com>
2021-10-26 01:00:41 +0300

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { useDispatchLoadAllData } from "./hooks/use-dispatch-load-all-data";
 import { useDispatchLoadPeople } from "./hooks/use-dispatch-load-people";
 import { useDispatchLoadPlanets } from "./hooks/use-dispatch-load-planets";
 
 export const App: React.FC = () => {
-  /* const store = useSelector((store) => store); */
+  const store = useSelector((store) => store);
+
+  useEffect(() => {
+    console.log(store);
+  }, [store]);
 
   const { dispatchLoadPeple } = useDispatchLoadPeople();
 
@@ -13,30 +19,34 @@ export const App: React.FC = () => {
   const { dispatchLoadAllData } = useDispatchLoadAllData();
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          dispatchLoadPeple();
-        }}
-      >
-        Load People
-      </button>
+    <>
+      <div>
+        <button
+          onClick={() => {
+            dispatchLoadPeple();
+          }}
+        >
+          Load People
+        </button>
 
-      <button
-        onClick={() => {
-          dispatchLoadPlanets();
-        }}
-      >
-        Load Planets
-      </button>
+        <button
+          onClick={() => {
+            dispatchLoadPlanets();
+          }}
+        >
+          Load Planets
+        </button>
 
-      <button
-        onClick={() => {
-          dispatchLoadAllData();
-        }}
-      >
-        Load All Planets
-      </button>
-    </div>
+        <button
+          onClick={() => {
+            dispatchLoadAllData();
+          }}
+        >
+          Load All Planets
+        </button>
+      </div>
+
+      <Link to="/Blog">Blog</Link>
+    </>
   );
 };
