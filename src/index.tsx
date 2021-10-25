d2814eec9178c8afa632d4160d9f603c6eaf0389
asosunoff <al.sosunoff@gmail.com>
2021-10-26 01:00:49 +0300

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app";
 import { Provider } from "react-redux";
-import { store } from "./redux";
+/* import { Router } from "react-router"; */
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+
+import { App } from "./app";
+import { configureStore } from "./redux";
+import { Blog } from "./feature/Blog";
+import { NotFound } from "./feature/NotFound";
+
+export const history = createBrowserHistory();
+
+const store = configureStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" exact>
+          <App />
+        </Route>
+
+        <Route path="/blog">
+          <Blog />
+        </Route>
+
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
