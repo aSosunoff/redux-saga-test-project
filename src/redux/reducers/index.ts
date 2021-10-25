0affb26e71effd255b72354f2827d631d378d421
asosunoff <al.sosunoff@gmail.com>
2021-10-26 01:01:30 +0300

    fix

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,50 +1,11 @@
-import { Reducer } from "redux";
-import { Person } from "../../interfaces/person";
-import { Planet } from "../../interfaces/planet";
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+import { History } from "history";
 
-type State = {
-  people: Person[];
-  planets: Planet[];
-};
+import { appReducer } from "./appReducer";
 
-const initialState: State = {
-  people: [],
-  planets: [],
-};
-
-/*  */
-type Action<T extends string> = {
-  type: T;
-};
-
-type ActionPayload<T extends string, P extends any> = Action<T> & {
-  payload: P;
-};
-/*  */
-
-export type ActionSetPeople = ActionPayload<"SET_PEOPLE", Person[]>;
-export type ActionSetPlanet = ActionPayload<"SET_PLANETS", Planet[]>;
-
-export const reducer: Reducer<State, ActionSetPeople | ActionSetPlanet> = (
-  state = initialState,
-  action
-) => {
-  switch (action.type) {
-    case "SET_PEOPLE": {
-      return {
-        ...state,
-        people: action.payload,
-      };
-    }
-
-    case "SET_PLANETS": {
-      return {
-        ...state,
-        planets: action.payload,
-      };
-    }
-
-    default:
-      return state;
-  }
-};
+export const createRootReducer = (history: History) =>
+  combineReducers({
+    app: appReducer,
+    router: connectRouter(history),
+  });
