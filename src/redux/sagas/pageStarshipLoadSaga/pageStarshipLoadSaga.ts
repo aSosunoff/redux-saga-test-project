ca533ce87f3f947b3c202323f89da4126f9fdc93
asosunoff <al.sosunoff@gmail.com>
2021-10-28 22:03:45 +0300

    fix

--- src/redux/sagas/pageStarshipLoadSaga/pageStarshipLoadSaga.ts
+++ src/redux/sagas/pageStarshipLoadSaga/pageStarshipLoadSaga.ts
@@ -6,7 +6,7 @@ export function* pageStarshipLoadSaga() {
   while (true) {
     const action: LocationChangeAction = yield take(LOCATION_CHANGE);
 
-    if (action.payload.location.pathname === "/starships") {
+    if (action.payload.location.pathname.endsWith("starships")) {
       yield fork(loadStarshipData);
     }
   }
