56787fe6daaa6f66cac18b9e74f739c1ce2df676
asosunoff <al.sosunoff@gmail.com>
2021-10-28 21:13:01 +0300

    fix

--- src/redux/sagas/pageStarshipLoadSaga/loadStarshipData.ts
+++ src/redux/sagas/pageStarshipLoadSaga/loadStarshipData.ts
@@ -2,8 +2,11 @@ import { Starship } from "./../../../interfaces/starship";
 import { call, put } from "redux-saga/effects";
 import { ActionSetStarship } from "../../reducers/appReducer";
 
-export function* loadStarshipData(): Generator<any, any, any> {
-  const request = yield call(fetch, "https://swapi.dev/api/starships/");
+export function* loadStarshipData() {
+  const request: Response = yield call(
+    fetch,
+    "https://swapi.dev/api/starships/"
+  );
 
   const { results } = yield call([request, request.json]);
 
