b2db2f102d76f219305a9f0794e0ece1962a3d34
asosunoff <al.sosunoff@gmail.com>
2021-10-28 21:12:17 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatchLoadAllData } from "./hooks/use-dispatch-load-all-data";
-import { useDispatchLoadPeople } from "./hooks/use-dispatch-load-people";
 import { useDispatchLoadPlanets } from "./hooks/use-dispatch-load-planets";
 
 export const App: React.FC = () => {
-  const { dispatchLoadPeple } = useDispatchLoadPeople();
-
   const { dispatchLoadPlanets } = useDispatchLoadPlanets();
 
   const { dispatchLoadAllData } = useDispatchLoadAllData();
@@ -14,14 +11,6 @@ export const App: React.FC = () => {
   return (
     <>
       <div>
-        <button
-          onClick={() => {
-            dispatchLoadPeple();
-          }}
-        >
-          Load People
-        </button>
-
         <button
           onClick={() => {
             dispatchLoadPlanets();
