bcdbc03d4ab1dd259ff26a31ee7733b8680fbcfa
asosunoff <al.sosunoff@gmail.com>
2021-10-28 21:35:38 +0300

    fix saga

--- src/redux/sagas/people/workeLoadPeopleSaga.ts
+++ src/redux/sagas/people/workeLoadPeopleSaga.ts
@@ -1,19 +1,13 @@
-import { put, call, PutEffect, CallEffect } from "redux-saga/effects";
+import { put, call } from "redux-saga/effects";
 import { Person } from "../../../interfaces/person";
 import { SWAPIService } from "../../../services/SWAPIService";
 import { ActionSetPeople } from "../../reducers/appReducer";
 
-export function* workeLoadPeopleSaga(
-  signal?: RequestInit["signal"]
-): Generator<
-  CallEffect<Person[]> | PutEffect<ActionSetPeople>,
-  void,
-  Person[]
-> {
-  const persons = yield call(SWAPIService.getPersons, { signal });
+export function* workeLoadPeopleSaga(signal?: RequestInit["signal"]) {
+  const persons: Person[] = yield call(SWAPIService.getPersons, { signal });
 
   yield put<ActionSetPeople>({
     type: "SET_PEOPLE",
-    payload: persons as Person[],
+    payload: persons,
   });
 }
